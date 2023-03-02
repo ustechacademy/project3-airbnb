75491d33711a6cd314a0ad8ed8f6a84fed83c125
ustechacademy <ustechacademytool@gmail.com>
2023-03-02 21:24:54 +0100

    Hero deployment

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ function Hero(){
     return(
         <section className="hero">
             <img src={heroImg} alt="hero-img" className="hero-img" />
-            <h1 className="hero-header">Online Experiences</h1>
+            <h1 className="hero-header">Online Experiences - DEPLOYMENT</h1>
             <p className="hero-text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home. </p>
         </section>
